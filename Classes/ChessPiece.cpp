390953160951b6a2f721ac3e34cb037355ebc97c
onlycxue <cxue189@gmail.com>
2014-01-20 17:38:02 +0800

    add logic function

--- Classes/ChessPiece.cpp
+++ Classes/ChessPiece.cpp
@@ -22,53 +22,74 @@ ChessPiece* ChessPiece::createWhitePiece()
 {
 	animationInit();
 
-	m_piece = CCSprite::create();
-	CCSpriteFrame *frame = m_frameCache->spriteFrameByName("");	
+	m_piece = ChessPiece::create();
+	CCSpriteFrame *frame = m_frameCache->spriteFrameByName("piece_01.png");	
 	m_piece->setDisplayFrame(frame);
-	m_pieceRole = WHITE;
-
-	return WhitePiece;
+	m_pieceRole = WHITESTATUS; 
+	return m_piece;
 }
 
 ChessPiece* ChessPiece::createBlackPiece()
 {
 	animationInit();
 
-	m_piece = CCSprite::create();
-	CCSpriteFrame *frame = m_frameCache->spriteFrameByName("");	
+	m_piece = ChessPiece::create();
+	CCSpriteFrame *frame = m_frameCache->spriteFrameByName("piece_02.png");	
 	m_piece->setDisplayFrame(frame);
-	m_pieceRole = BLACK;
+	m_pieceRole = BLACKSTATUS;
 
-	return BlackPiece;
-
-}
-
+	return m_piece;
+} 
 void ChessPiece::animationInit()
 {
 	m_frameCache = CCSpriteFrameCache::sharedSpriteFrameCache();
 	m_frameCache->addSpriteFramesWithFile("");
 	
-	CCAnimationCache::purgeSharedAnimationCache();
-	CCAnimationCache *animCache = CCAnimationCache::sharedAnimationCache();
-	animCache->addAnimationsWithFile("");
-
-	CCAnimation* whiteToBlack = animCache->animationByName(); 
-	CCAnimation* blackToWhite = animCache->animationByName();
+//	CCAnimationCache::purgeSharedAnimationCache();
+//	CCAnimationCache *animCache = CCAnimationCache::sharedAnimationCache();
+//	animCache->addAnimationsWithFile("");
+	CCArray* animFrames = CCArray::createWithCapacity(2);
+	char str[100];
+	for(int i = 1;i < 3; i++ )
+	{
+		sprintf(str,"piece_%02d.png",i);
+		CCSpriteFrame *frame = m_frameCache->spriteFrameByName(str); animFrames->addObject(frame);
+	}
+	CCAnimation *whiteToBlack = CCAnimation::createWithSpriteFrames(animFrames,0.2f);
+	CCAnimationCache::sharedAnimationCache()->addAnimation(whiteToBlack,"whiteToBlack");
+	animFrames->removeAllObjects();
 	
-	m_WhiteToBlack = CCAnimate::create(whiteToBlack);
-	m_BlackToWhite = CCAnimate::create(blackToWhite);
+	for(int i = 2; i > 0; i--)
+	{
+		sprintf(str,"piece_%02d.png",i);
+		CCSpriteFrame * frame = m_frameCache->spriteFrameByName(str);
+		animFrames->addObject(frame);
+	
+	}
+	CCAnimation *blackToWhite = CCAnimation::createWithSpriteFrames(animFrames,0.2f);
+	CCAnimationCache::sharedAnimationCache()->addAnimation(blackToWhite,"blackToWhite");
+	animFrames->removeAllObjects();
+
+//	CCAnimation* whiteToBlack = animCache->animationByName(); 
+//	CCAnimation* blackToWhite = animCache->animationByName();
+	CCAnimationCache *animCache = CCAnimationCache::sharedAnimationCache();
+	CCAnimation* btw = animCache->animationByName("blackToWhite");
+	CCAnimation* wtb = animCache->animationByName("whiteToBlack");
+
+	m_WhiteToBlack = CCAnimate::create(wtb);
+	m_BlackToWhite = CCAnimate::create(btw);
 
 }
-void ChessPiece::changeRole(enum Role role)
+void ChessPiece::changeRole(enum PieceStatus role)
 {
 	//change to role
 	if(m_pieceRole != role)
 	{
-		if(m_pieceRole == BLACK)			
+		if(m_pieceRole == BLACKSTATUS)			
 		{
 			m_piece->runAction(m_WhiteToBlack);	
 		}
-		else if(m_pieceRole == WHITE)
+		else if(m_pieceRole == WHITESTATUS)
 		{
 			m_piece->runAction(m_BlackToWhite);	
 		}
